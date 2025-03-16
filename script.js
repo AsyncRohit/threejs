f6e53fa889055a155c828089831d0f3e06ae39ad
Rohit <heartstealarrohitthakur@gmail.com>
2025-03-16 23:56:17 +0530

    handling window resize

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// Creating the scene
+let scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+// Creating the camera
+let camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
 camera.position.z = 5;
+scene.add(camera);
+
+// Creating geometry
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({ color: "red" });
+let mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Creating the renderer
 const canvas = document.querySelector("#draw");
-const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize( window.innerWidth, window.innerHeight );
+let renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+// Clock for animation
+const clock = new THREE.Clock();
+
 function animate() {
-    window.requestAnimationFrame(animate)
-	renderer.render( scene, camera );
-    cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
+  window.requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  
+  // Updating mesh rotation
+  mesh.rotation.y = clock.getElapsedTime();
 }
+
 animate();
+
+// Handle window resize
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
